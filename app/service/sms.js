dbd2bb3044df86ce32bc250e991c9a02c3445120
aojiaotage <aojiaotage@gmail.com>
2019-07-26 01:27:19 +0800

    Add SMS login

---
+++ app/service/sms.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { Service } = require('egg');
+
+class User extends Service {
+  async createUserWithUnPw(username, password) {
+    const created = await this.ctx.model.User.createUserWithUnPw(username,
+      password);
+    return created;
+  }
+
+  async loginWithUnPw(username, password) {
+    const found = await this.ctx.model.User.loginWithUnPw(username, password);
+    return found;
+  }
+
+  async getUserInfo(userId, opts) {
+    const { isUserHimself } = opts;
+    const select = ['id'];
+    if (isUserHimself) {
+      select.push('phoneNumber', 'email', 'phoneNumber', 'username', 'icon');
+    }
+    const user = await this.ctx.model.User.getUser(userId, select);
+    return user;
+  }
+
+  async updateUser(userId, user) {
+    const r = await this.ctx.model.User.updateUser(userId, user);
+    return r;
+  }
+}
+
+module.exports = User;
